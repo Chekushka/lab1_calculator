0486c8ec42e969dfae91dcc130b752af9ca16451
Chekushka <chekun.sergiy@gmail.com>
2021-11-09 20:31:52 +0200

    Project initialization

---
+++ Lab1_CalculatorV2/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1_CalculatorV2
+{
+    public partial class Calculator : Form
+    {
+        private readonly CalculatorLogic _logic;
+
+        public Calculator()
+        {
+            _logic = new CalculatorLogic();
+            InitializeComponent();
+        }
+
+        private void NumberButtonClick(object sender, EventArgs e)
+        {
+            if ((textBox_Result.Text == "0") || (_logic.IsOperationPerformed))
+                textBox_Result.Clear();
+
+            _logic.IsOperationPerformed = false;
+            var button = (Button)sender;
+            if (button.Text == ".")
+            { 
+               if(!textBox_Result.Text.Contains("."))
+                   textBox_Result.Text += button.Text;
+            }
+            else
+             textBox_Result.Text += button.Text;
+        }
+
+        private void OperatorButtonClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (_logic.ResultValue != 0)
+            {
+                _logic.OperationPerformed = button.Text;
+                labelCurrentOperation.Text = _logic.ResultValue + @" " + _logic.OperationPerformed;
+                _logic.IsOperationPerformed = true;
+                textBox_Result.Text = _logic.ClearTextField();
+            }
+            else
+            {
+                _logic.OperationPerformed = button.Text;
+                _logic.ResultValue = double.Parse(textBox_Result.Text);
+                labelCurrentOperation.Text = _logic.ResultValue + @" " + _logic.OperationPerformed;
+                _logic.IsOperationPerformed = true;
+            }
+        }
+
+        private void ClearButtonClick(object sender, EventArgs e) => ClearField();
+
+        private void SaveButtonClick(object sender, EventArgs e)
+        {
+            _logic.SaveValue();
+            _logic.ResetResultValue();
+            textBox_Result.Text = _logic.ClearTextField();
+        }
+
+        private void LoadButtonClick(object sender, EventArgs e) => textBox_Result.Text = _logic.LoadValue().ToString();
+
+        private void EqualsButtonClick(object sender, EventArgs e)
+        {
+            _logic.PerformOperation(textBox_Result);
+            labelCurrentOperation.Text = "";
+        }
+
+        private void ClearField()
+        {
+            textBox_Result.Text = _logic.ClearTextField();
+            labelCurrentOperation.Text = "";
+            _logic.ResultValue = 0;
+        }
+    }
+}
---
+++ Lab1_CalculatorV2/CalculatorLogic.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+
+namespace Lab1_CalculatorV2
+{
+    public class CalculatorLogic
+    {
+        public bool IsOperationPerformed { get; set; }
+        public double ResultValue { get; set; }
+        public string OperationPerformed { get; set; } = "";
+
+        private double SavedValue { get; set; }
+        
+        public string ClearTextField() => @"0";
+        public void ResetResultValue() => ResultValue = 0;
+
+        public void SaveValue() => SavedValue = ResultValue;
+        
+        public double LoadValue() => SavedValue;
+        public string GetResultValueString() => ResultValue.ToString();
+
+        public void PerformOperation(TextBox textBox)
+        {
+            switch (OperationPerformed)
+            {
+                case "+":
+                    textBox.Text = (ResultValue + double.Parse(textBox.Text)).ToString();
+                    break;
+                case "-":
+                    textBox.Text = (ResultValue - double.Parse(textBox.Text)).ToString();
+                    break;
+                case "*":
+                    textBox.Text = (ResultValue * double.Parse(textBox.Text)).ToString();
+                    break;
+                case "/":
+                    textBox.Text = (ResultValue / double.Parse(textBox.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            
+            ResultValue = double.Parse(textBox.Text);
+        }
+    }
+}
